2714d13772e0871c595439afd868d10980bf5646
NLSanyu <lydiansanyu@gmail.com>
2022-01-17 20:02:12 +0300

    Add noise measurement selection

---
+++ src/components/NoiseRange/NoiseRange.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const NoiseRangeContainer = styled.div`
+    justifyContent: center;
+    margin-left: 2em;
+    margin-bottom: 4em;
+    width: 95%;
+`;
