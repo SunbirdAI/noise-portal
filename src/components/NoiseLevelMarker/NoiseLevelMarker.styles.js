e7e88a1f327eb161640be992bd34d11122074eab
Isaac Owomugisha <archimedesisaac@gmail.com>
2022-02-10 22:54:50 +0300

    Add custom popup

--- src/components/NoiseLevelMarker/NoiseLevelMarker.styles.js
+++ src/components/NoiseLevelMarker/NoiseLevelMarker.styles.js
@@ -17,3 +17,54 @@ export const NoiseLevelMarkerContainer = styled.div`
     h-[50px]
   `}
 `;
+
+export const PopUpContainer = styled.div`
+  ${tw`
+        grid
+        grid-cols-1
+        gap-2
+        bg-white
+        justify-center
+        justify-items-center
+        w-[150px]
+        h-[70px]
+        md:h-[100px]
+    `}
+`;
+
+export const LocationText = styled.div`
+  ${tw`    
+    text-lg
+    text-center
+    grid
+    grid-cols-1
+    content-center
+   `}
+`;
+
+export const NoiseLevelDescription = styled.div`
+  ${props => noiseColors[props.color_id]}
+  ${props => props.color_id >= 8 ? redText : blackText}
+  ${tw`
+    flex
+    justify-center
+    items-center
+    justify-items-center
+    gap-4
+    p-2
+    text-sm
+    `}
+`;
+
+export const ViewLocationButton = styled.button`
+  ${tw`
+    shadow
+    bg-sunbird-light-orange
+    ring-1
+    ring-sunbird-dark-orange
+    rounded-2xl
+    text-opacity-90
+    hover:bg-sunbird-dark-orange
+    w-3/4
+  `}
+`;
