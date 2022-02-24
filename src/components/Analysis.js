6493d98db393954dfe33835f64ed150d5329f2db
Isaac Owomugisha <archimedesisaac@gmail.com>
2022-02-24 19:37:06 +0300

    Add analysis charts

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -2,6 +2,8 @@ import {AnalysisWrapper, Wrapper} from "../GlobalStyles";
 import LocationFilter from "./LocationFilter";
 import Intro from "./Intro";
 import NoiseStatisticCard from "./NoiseStatisticCard";
+import {AnalysisBarChartsContainer} from "./AnalysisBarChart/AnalysisBarChart.styles";
+import AnalysisBarChart from "./AnalysisBarChart";
 
 const introText = "Select a city to view a summary of the noise levels in this city";
 
@@ -15,6 +17,10 @@ const Analysis = () => (
             <NoiseStatisticCard title={"Quiet Areas"} value={8} noise_range={0}/>
             <NoiseStatisticCard title={"Moderately Noisy Areas"} value={8} noise_range={1}/>
             <NoiseStatisticCard title={"Very Noisy Areas"} value={8} noise_range={2}/>
+            <AnalysisBarChartsContainer>
+                <AnalysisBarChart title='Daily Average' metric='average_noise'/>
+                <AnalysisBarChart title='Total Exceedances' metric='total_exceedances'/>
+            </AnalysisBarChartsContainer>
         </AnalysisWrapper>
     </>
 );
---
+++ src/components/AnalysisBarChart/index.js
@@ -0,0 +1,58 @@
+import {ChartContainer} from "../NoiseCategoryChart/NoiseCategoryChart.styles";
+import {CardTitle} from "../NoiseLevelCard/LocationCard.styles";
+import {LineGraphContainer} from "../NoiseLevelChart/NoiseLevelChart.styles";
+import {ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, Legend, Bar} from "recharts";
+
+const data = [
+   {
+	"city": "Kampala",
+	"division": "Makindye",
+	"average_noise": 65,
+	"total_exceedances": 900
+   },
+   {
+	"city": "Kampala",
+	"division": "Central",
+	"average_noise": 78,
+	"total_exceedances": 650
+   },
+   {
+	"city": "Kampala",
+	"division": "Nakawa",
+	"average_noise": 56,
+	"total_exceedances": 100
+   },
+   {
+	"city": "Kampala",
+	"division": "Kawempe",
+	"average_noise": 45,
+	"total_exceedances": 500
+   },
+   {
+	"city": "Kampala",
+	"division": "Rubaga",
+	"average_noise": 80,
+	"total_exceedances": 432
+   }
+];
+
+const AnalysisBarChart = ({title, metric}) => (
+	<ChartContainer className='w-full'>
+		<CardTitle>{title}</CardTitle>
+		<LineGraphContainer>
+			<ResponsiveContainer width='95%' height='80%'>
+				<BarChart
+					data={data}
+				>
+					<XAxis dataKey='division'/>
+					<YAxis/>
+					<Tooltip/>
+					<Legend/>
+					<Bar dataKey={metric} fill='#8884d8'/>
+				</BarChart>
+			</ResponsiveContainer>
+		</LineGraphContainer>
+	</ChartContainer>
+);
+
+export default AnalysisBarChart;
