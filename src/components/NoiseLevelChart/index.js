06ed52f4cd7ac124c64a337fdd145de50fb89eb5
Isaac Owomugisha <archimedesisaac@gmail.com>
2022-03-03 00:31:43 +0300

    Add tooltip to noise level graph

--- src/components/NoiseLevelChart/index.js
+++ src/components/NoiseLevelChart/index.js
@@ -1,6 +1,9 @@
 import {ChartContainer, LineGraphContainer} from "./NoiseLevelChart.styles";
 import {CardTitle} from "../NoiseLevelCard/LocationCard.styles";
-import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, ReferenceLine} from "recharts";
+import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, ReferenceLine, Tooltip} from "recharts";
+import {timeFormat} from "d3-time-format";
+
+const fullDateFormat = timeFormat("%a %d %b %Y %I:%M %p");
 
 // const formatDate = (dateString) =>  {
 //     const date = new Date(dateString);
@@ -18,6 +21,7 @@ const NoiseLevelChart = ({metrics, timeFormat}) => (
                     {/*<XAxis angle={315} dy={10} ticks={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}/>*/}
                     <YAxis/>
                     <ReferenceLine y={70} label="High noise" stroke="red"/>
+                    <Tooltip labelFormatter={fullDateFormat}/>
                     <Line type="monotone" dataKey="avg_db_level" stroke="#8884d8"/>
                 </LineChart>
             </ResponsiveContainer>
