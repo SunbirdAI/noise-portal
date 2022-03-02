f001220bc308893b61005fc5d9dab88cda8e47f0
Isaac Owomugisha <archimedesisaac@gmail.com>
2022-03-03 00:10:26 +0300

    Add date filtering capabilities

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,22 +1,98 @@
 import {AnalysisWrapper} from "../GlobalStyles";
 import NoiseLevelCard, {GenericNumberCard} from "./NoiseLevelCard";
 import {useLocation} from "react-router-dom";
-import {LocationNameText} from "./NoiseLevelCard/LocationCard.styles";
+import {CardTitle, GenericNumberCardContainer, LocationNameText} from "./NoiseLevelCard/LocationCard.styles";
 import NoiseLevelChart from "./NoiseLevelChart";
 import NoiseCategoryChart from "./NoiseCategoryChart";
+import {useState} from "react";
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import DefinedRange from "react-date-range/dist/components/DefinedRange";
+import {timeFormat} from 'd3-time-format';
+
+const today = new Date();
+const pastDay = new Date();
+pastDay.setDate(new Date().getDate() - 1);
+const hourFormat = timeFormat("%a %I:%M %p");
+const dayFormat = timeFormat("%a %d %b");
+
+
+const daysBetween = (firstDate, secondDate) => (
+    Math.round(Math.abs(firstDate.getTime() - secondDate.getTime()) / (24 * 60 * 60 * 1000))
+);
+
+const getTimeFormat = (startDate, endDate) => {
+    const numOfDaysBetween = daysBetween(startDate, endDate);
+    if (numOfDaysBetween <= 2) return hourFormat;
+    else return dayFormat;
+}
+
+const filterMetrics = (metrics, startDate, endDate) => {
+    const start = startDate.getTime();
+    const end = endDate.getTime();
+
+    return metrics.filter(metric => metric.time_uploaded >= start && metric.time_uploaded <= end)
+}
+
+const getLatestMetric = (metrics) => (metrics.reduce((prev, current) =>
+    prev.time_uploaded > current.time_uploaded ? prev : current
+));
+
+const getTotalExceedances = (metrics) =>
+    (metrics.reduce((prev, current) => prev + current.no_of_exceedances, 0));
 
 const Location = () => {
     const route = useLocation();
-    const { location } = route.state;
-    console.log(route.state);
+    const {location} = route.state;
+    const [dateState, setDateState] = useState([
+        {
+            startDate: pastDay,
+            endDate: today,
+            key: 'selection'
+        }
+    ]);
+
+    const initialMetrics = filterMetrics(location.metrics, pastDay, today);
+
+    const [metricsState, setMetricsState] = useState({
+        metrics: initialMetrics,
+        timeFormat: getTimeFormat(pastDay, today),
+        latestMetric: getLatestMetric(location.metrics),
+        totalExceedances: getTotalExceedances(initialMetrics)
+    });
+
+    const onDateFilterChanged = (item) => {
+        const startDate = item.selection.startDate, endDate = item.selection.endDate;
+        setDateState([item.selection]);
+        const newMetrics = filterMetrics(location.metrics, startDate, endDate);
+        setMetricsState({
+            ...metricsState,
+            metrics: newMetrics,
+            timeFormat: getTimeFormat(startDate, endDate),
+            totalExceedances: getTotalExceedances(newMetrics)
+        });
+    }
 
     return (
         <AnalysisWrapper>
             <LocationNameText>{location.name}</LocationNameText>
-            <NoiseLevelCard location={location} title={"Average (Past 30 minutes)"}/>
-            <GenericNumberCard title={"Exceedances"} value={8}/>
-            <NoiseLevelCard location={location} title={"Maximum"}/>
-            <NoiseLevelChart metrics={location.metrics}/>
+            <NoiseLevelCard value={Math.round(metricsState.latestMetric.avg_db_level)}
+                            title={"Average (Past 30 minutes)"}/>
+            <NoiseLevelCard value={Math.round(metricsState.latestMetric.max_db_level)}
+                            title={"Maximum (Past 30 minutes)"}/>
+            <GenericNumberCard title={"Exceedances (For time Period Selected)"} value={metricsState.totalExceedances}/>
+            <GenericNumberCardContainer>
+                <CardTitle>Choose a time range</CardTitle>
+                <DefinedRange
+                    onChange={onDateFilterChanged}
+                    // showSelectionPreview={true}
+                    // moveRangeOnFirstSelection={false}
+                    // months={1}
+                    ranges={dateState}
+                    direction="horizontal"
+                />
+            </GenericNumberCardContainer>
+            <NoiseLevelChart metrics={metricsState.metrics} timeFormat={metricsState.timeFormat}/>
             <NoiseCategoryChart/>
         </AnalysisWrapper>
     )
--- src/components/NoiseLevelChart/index.js
+++ src/components/NoiseLevelChart/index.js
@@ -1,27 +1,24 @@
 import {ChartContainer, LineGraphContainer} from "./NoiseLevelChart.styles";
 import {CardTitle} from "../NoiseLevelCard/LocationCard.styles";
 import {ResponsiveContainer, LineChart, Line, XAxis, YAxis, ReferenceLine} from "recharts";
-import {timeFormat} from 'd3-time-format';
 
-const format = timeFormat("%I:%M %p");
-const dayFormat = timeFormat("%a %d %b");
-const formatDate = (dateString) =>  {
-    const date = new Date(dateString);
-    return format(date);
-}
+// const formatDate = (dateString) =>  {
+//     const date = new Date(dateString);
+//     return hourFormat(date);
+// }
 
 
-const NoiseLevelChart = ({metrics}) => (
+const NoiseLevelChart = ({metrics, timeFormat}) => (
     <ChartContainer>
-        <CardTitle>Noise Levels Overtime</CardTitle>
+        <CardTitle>Noise Levels Overtime (30 minute average)</CardTitle>
         <LineGraphContainer>
             <ResponsiveContainer width="95%" height="100%">
                 <LineChart data={metrics}>
-                    <XAxis type="number" domain={['dataMin', 'dataMax']} dataKey="time_uploaded" tickFormatter={dayFormat}/>
+                    <XAxis type="number" domain={['dataMin', 'dataMax']} dataKey="time_uploaded" tickFormatter={timeFormat}/>
                     {/*<XAxis angle={315} dy={10} ticks={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}/>*/}
                     <YAxis/>
                     <ReferenceLine y={70} label="High noise" stroke="red"/>
-                    <Line type="monotone" dataKey="db_level" stroke="#8884d8"/>
+                    <Line type="monotone" dataKey="avg_db_level" stroke="#8884d8"/>
                 </LineChart>
             </ResponsiveContainer>
         </LineGraphContainer>
