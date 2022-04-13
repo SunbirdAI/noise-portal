2dec86a7b087a873ab21fc0e7b051c0d8a0a8440
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-04-13 09:09:07 +0300

    Fetch analysis from API and pass it to analysis component

--- src/API.js
+++ src/API.js
@@ -4,6 +4,8 @@ const API_URL = process.env.REACT_APP_API_URL;
 
 const LOCATIONS_URL = `${API_URL}/devices/locations`;
 
+const ANALYSIS_URL = `${API_URL}/analysis`;
+
 const getLocationMetricsURL = (locationId) => (`${API_URL}/devices/location_metrics/${locationId}`);
 
 const fetchLocations = async () => {
@@ -17,9 +19,14 @@ const fetchLocationMetrics = async (locationId) => {
     return metrics['location_metrics'];
 }
 
+const fetchAnalysis = async () => {
+    return await (await fetch(ANALYSIS_URL)).json();
+}
+
 export {
     LOCATIONS_URL,
     fetchLocations,
     fetchLocationMetrics,
+    fetchAnalysis,
     getLocationMetricsURL
 }
