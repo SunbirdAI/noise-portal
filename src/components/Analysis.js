4d9edbdc73719f99379c8cf79e42bb50dae4536f
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-04-20 15:19:37 +0300

    Add day and night time distinction on graph

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -18,7 +18,8 @@ const Analysis = () => {
         const analysis = response.map(({ location: { parish, noise_analysis} }) => ({
             day_time_average: noise_analysis.day_time_average,
             day_time_median: noise_analysis.day_time_median,
-            // division: parish ? `${parish}, ${division}` : division,
+            night_time_average: noise_analysis.night_time_average,
+            night_time_median: noise_analysis.night_time_median,
             parish: parish
         }));
         setAnalysis(analysis);
@@ -41,8 +42,14 @@ const Analysis = () => {
                 <NoiseStatisticCard title={"Moderately Noisy Areas"} value={8} noise_range={1}/>
                 <NoiseStatisticCard title={"Very Noisy Areas"} value={8} noise_range={2}/>
                 <AnalysisBarChartsContainer>
-                    <AnalysisBarChart title='Daily Average' metric='day_time_average' analysis={analysis}/>
-                    <AnalysisBarChart title='Daily Median' metric='day_time_median' analysis={analysis}/>
+                    <AnalysisBarChart title='Noise Analysis (3-day period)'
+                        metric_1='day_time_average'
+                        metric_2='day_time_median'
+                        metric_3='night_time_average'
+                        metric_4='night_time_median'
+                        analysis={analysis}
+                    />
+                    {/* <AnalysisBarChart title='Exceedances' metric='excedances'/> */}
                 </AnalysisBarChartsContainer>
             </AnalysisWrapper>
         </>
--- src/components/AnalysisBarChart/index.js
+++ src/components/AnalysisBarChart/index.js
@@ -4,7 +4,7 @@ import {LineGraphContainer} from "../NoiseLevelChart/NoiseLevelChart.styles";
 import {ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, Legend, Bar} from "recharts";
 
 
-const AnalysisBarChart = ({title, metric, analysis}) => (
+const AnalysisBarChart = ({title, metric_1, metric_2, metric_3, metric_4, analysis}) => (
 	<ChartContainer className='w-full'>
 		<CardTitle>{title}</CardTitle>
 		<LineGraphContainer>
@@ -16,7 +16,10 @@ const AnalysisBarChart = ({title, metric, analysis}) => (
 					<YAxis/>
 					<Tooltip/>
 					<Legend/>
-					<Bar dataKey={metric} fill='#8884d8'/>
+					<Bar dataKey={metric_1} fill='#b8d6d1'/>
+					<Bar dataKey={metric_2} fill='#82a6ad'/>
+					<Bar dataKey={metric_3} fill='#75085c'/>
+					<Bar dataKey={metric_4} fill='#430a4a'/>
 				</BarChart>
 			</ResponsiveContainer>
 		</LineGraphContainer>
