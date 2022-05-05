1abb08f87094ce2511f15eab55d6096c97273f3c
Lydia Sanyu Naggayi <37118361+NLSanyu@users.noreply.github.com>
2022-05-05 16:41:43 +0300

    Add exceedances graph to analysis page

---
+++ src/components/ExceedanceBarChart/ExceedanceBarChart.styles.js
@@ -0,0 +1,15 @@
+import tw, {styled} from "twin.macro";
+
+
+export const ExceedanceBarChartsContainer = styled.div`
+    ${tw`
+        grid
+        grid-cols-1
+        gap-4
+        md:col-span-3
+        md:grid-cols-2
+        justify-items-center
+        content-around
+        rounded-lg
+    `}
+`;
